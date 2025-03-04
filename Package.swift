5c12d6c16e08a7e136840a11532eeef19d1310cb
Matheus Silveira Venturini <msventurini@icloud.com>
2025-03-04 20:04:33 -0300

    added xcframework build and swift package manifest

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
    targets: [
       .binaryTarget(
          name: "Shared",
-         url: "https://github.com/msventurini/VentuKitNetworking/releases/download/0.0.5/VentuKitNetworking.xcframework.zip",
+         url: "https://github.com/msventurini/VentuKitNetworking/releases/download/0.0.6/VentuKitNetworking.xcframework.zip",
          checksum:"3e6ab47e267206aae89dbdbad9a664c26177a4e7979ce5127766e03e9ef5fe8a")
    ]
 )
