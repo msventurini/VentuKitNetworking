348b4ddf4af64fc2a4b0b37ff0b0357cb6a88bda
Matheus Silveira Venturini <msventurini@icloud.com>
2025-03-05 04:44:18 -0300

    added basic network request

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -43,7 +43,6 @@ kotlin {
 
         commonMain.dependencies {
             implementation(libs.ktor.client.core)
-            implementation(libs.ktor.client.core)
             implementation(libs.kotlinx.coroutines.core)
         }
 
