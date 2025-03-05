c08a0cfd51814201d0a476424768a7046224fa8b
Matheus Silveira Venturini <msventurini@icloud.com>
2025-03-05 09:29:03 -0300

    added iOS sample app.

---
+++ sampleApps/IOSSample/IOSSample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IOSSample
+//
+//  Created by Matheus Silveira Venturini on 05/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
