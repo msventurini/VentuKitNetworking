c68b7da3fe34dd51599ed5d6f3ad461ccffbe575
Matheus Silveira Venturini <msventurini@icloud.com>
2025-04-08 22:42:25 -0300

    updated amper setup

--- library/src/androidMain/kotlin/ventukitnetworking/PlatformData.android.kt
+++
@@ -1,8 +0,0 @@
-package ventukitnetworking
-
-class AndroidPlatform: PlatformData {
-    override val name: String =
-        "Android ${android.os.Build.VERSION.SDK_INT}"
-}
-
-actual fun getPlatform(): PlatformData = AndroidPlatform()
